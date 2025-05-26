36e23213e5f210e4db030e28b3f64f7201adf63b
twopigoens <seanryder264@outlook.com>
2025-05-26 23:13:41 +0100

    not sure why its looking like that but seperated the image builder

--- FPGA_sim/wrapper.cpp
+++ FPGA_sim/wrapper.cpp
@@ -9,10 +9,7 @@ int main(int argc, char** argv) {
     contextp->commandArgs(argc, argv);
     Vpixel_generator* top = new Vpixel_generator{contextp};
 
-    std::ofstream out("out_image.ppm");
-    out << "P3\n";
-    out << "640 480\n";
-    out << "255\n";
+    std::ofstream raw_data("data_stream.bin", std::ios::binary);
 
     top->axi_resetn = 0;
     top->periph_resetn = 0;
@@ -40,43 +37,8 @@ int main(int argc, char** argv) {
         top->eval();
 
         if (top->out_stream_tvalid && top->out_stream_tready) {
-            buffer.push_back(static_cast<uint32_t>(top->out_stream_tdata));
-
-            if (buffer.size() == 3) {
-                uint32_t w1 = buffer[0];
-                uint32_t w2 = buffer[1];
-                uint32_t w3 = buffer[2];
-
-                // Extract and write each of the 4 pixels
-                uint8_t r0 = (w1 >> 24) & 0xFF;
-                uint8_t g0 = (w1 >> 16) & 0xFF;
-                uint8_t b0 = (w1 >> 8)  & 0xFF;
-                uint8_t r1 = (w1)       & 0xFF;
-
-                uint8_t g1 = (w2 >> 24) & 0xFF;
-                uint8_t b1 = (w2 >> 16) & 0xFF;
-                uint8_t r2 = (w2 >> 8)  & 0xFF;
-                uint8_t g2 = (w2)       & 0xFF;
-
-                uint8_t b2 = (w3 >> 24) & 0xFF;
-                uint8_t r3 = (w3 >> 16) & 0xFF;
-                uint8_t g3 = (w3 >> 8)  & 0xFF;
-                uint8_t b3 = (w3)       & 0xFF;
-
-                auto write_pixel = [&](uint8_t r, uint8_t g, uint8_t b) {
-                    if (pixel_count < max_pixels) {
-                        out << (int)r << " " << (int)g << " " << (int)b << "\n";
-                        pixel_count++;
-                    }
-                };
-
-                write_pixel(r0, g0, b0);
-                write_pixel(r1, g1, b1);
-                write_pixel(r2, g2, b2);
-                write_pixel(r3, g3, b3);
-
-                buffer.clear();
-            }
+            uint32_t word = top->out_stream_tdata;
+            raw_data.write(reinterpret_cast<const char*>(&word), sizeof(word));
         }
 
         top->out_stream_aclk = 0;
@@ -84,7 +46,6 @@ int main(int argc, char** argv) {
         top->eval();
     }
 
-    out.close();
     delete top;
     return 0;
 }
