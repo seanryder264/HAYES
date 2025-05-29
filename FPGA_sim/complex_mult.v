4abb3a755733c5f09aa6723fe4da820598c22586
twopigoens <seanryder264@outlook.com>
2025-05-29 17:39:16 +0100

    two zeros

---
+++ FPGA_sim/complex_mult.v
@@ -0,0 +1,13 @@
+module complex_mult(
+    input [15:0]    a_re,
+    input [15:0]    a_im,
+    input [15:0]    b_re,
+    input [15:0]    b_im,
+    output [15:0]   c_re,
+    output [15:0]   c_im
+);
+
+assign c_re = a_re * b_re - a_im * b_im;
+assign c_im = a_im * b_re + a_re * b_im;
+
+endmodule
--- FPGA_sim/func_eval.v
+++ FPGA_sim/func_eval.v
@@ -5,7 +5,25 @@ module func_eval(
     output [15:0]   w_im
 );
 
-assign w_re = z_re;
-assign w_im = z_im;
+reg [31:0] zeros [0:1];
+
+assign zeros[0] = {16'd0, 16'd0};
+assign zeros[1] = {-16'd0, -16'd0};
+
+wire [15:0] a1_re, a1_im; 
+
+complex_sub sub_one (   .a_re(z_re), .a_im(z_im),
+                            .b_re(zeros[0][15:0]), .b_im(zeros[0][31:16]),
+                            .c_re(a1_re), .c_im(a1_im));
+
+wire [15:0] a2_re, a2_im; 
+
+complex_sub sub_two (   .a_re(z_re), .a_im(z_im),
+                            .b_re(zeros[1][15:0]), .b_im(zeros[1][31:16]),
+                            .c_re(a2_re), .c_im(a2_im));       
+
+complex_mult mult_one ( .a_re(a1_re), .a_im(a1_im),
+                        .b_re(a2_re), .b_im(a2_im),
+                        .c_re(w_re), .c_im(w_im));     
 
 endmodule
